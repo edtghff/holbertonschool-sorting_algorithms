a14861f7d0c256b626a501a51278bc66d644b736
Ahmed <edtaghiyev@gmail.com>
2024-12-15 18:38:49 +0400

    2-O Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,12 +25,14 @@ void selection_sort(int *array, size_t size)
 				min_index = j;
 			}
 		}
+        if (min_index != i)
+        {
 	temp = array[i];
 	array[i] = array[min_index];
 	array[min_index] = temp;
 	print_array(array, size);
 
 	}
-
+    }
 
 }
