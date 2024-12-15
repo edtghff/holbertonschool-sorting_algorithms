60798c29724dc1863b498b18864e4c14a637f5c0
damaske <damaske.vzk@gmail.com>
2024-12-15 14:42:49 +0000

    hmm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (arra[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
