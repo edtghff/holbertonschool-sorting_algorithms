de685358ea53b390a0aad8e3826b199da6c65926
Ahmed <edtaghiyev@gmail.com>
2024-12-15 18:33:53 +0400

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - impelentation of selection sorting algorithm
+ * @array: array
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+	temp = array[i];
+	array[i] = array[min_index];
+	array[min_index] = temp;
+	print_array(array, size);
+
+	}
+
+
+}
